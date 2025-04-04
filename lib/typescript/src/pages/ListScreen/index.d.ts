250a3d4311b6d7d8f6534ef8012525f4fff1664f
arianshi <arian.shi@outlook.com>
2025-04-05 02:43:31 +0800

    feat: add libs

---
+++ lib/typescript/src/pages/ListScreen/index.d.ts
@@ -0,0 +1,3 @@
+import React from 'react';
+declare const ListScreen: () => React.JSX.Element;
+export default ListScreen;
---
+++ lib/typescript/src/utils/constants.d.ts
@@ -0,0 +1,3 @@
+export declare const ROUTES: {
+    readonly LIST_SCREEN: "ListScreen";
+};
