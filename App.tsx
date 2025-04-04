5e3541436ff1b4f4ed52bcac4efd66ca62335869
Arian <arian.shi@outlook.com>
2025-04-04 23:09:55 +0800

    Infra:builder app (#1)

--- App.tsx
+++ App.tsx
@@ -1,11 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
+
 import type {PropsWithChildren} from 'react';
 import {
   ScrollView,
@@ -98,9 +92,6 @@ function App(): React.JSX.Element {
           </Section>
           <Section title="Debug">
             <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
           </Section>
           <LearnMoreLinks />
         </View>
---
+++ pull_request_template.md
@@ -0,0 +1,9 @@
+## Describe your changes
+
+## Issue ticket number and link
+
+[REP-###]
+
+## Checklist before requesting a review
+
+- [ ] I have performed a self-review of my code
