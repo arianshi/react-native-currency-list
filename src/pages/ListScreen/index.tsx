c293cc309d213e8ff611999092ca55be8ea3f0a4
Arian <arian.shi@outlook.com>
2025-04-06 20:31:11 +0800

    feat: add react native vector icons (#4)
    * feat: add icon and empty state
    
    * feat: fix android issues

--- src/pages/ListScreen/index.tsx
+++ src/pages/ListScreen/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import {
   View,
   Text,
@@ -7,10 +7,13 @@ import {
 } from 'react-native';
 
 import useCurrencyListContext from '../../hooks/useCurrencyListContext';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { EmptyState } from '../../components/EmptyState';
 
-import styles from './styles';
 import { SearchBar } from '../../components/SearchBar/index';
 import { useCurrencySearch } from '../../hooks/useCurrencySearch';
+
+import styles from './styles';
 
 export default function CurrencyList() {
   const {
@@ -20,11 +23,18 @@ export default function CurrencyList() {
   const [search, setSearch] = useState('');
   const filtered = useCurrencySearch(data, search);
 
+  const handleClear = useCallback(() => {
+    setSearch('');
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{'List Cryptocurrency'}</Text>
-      <SearchBar value={search} onChangeText={setSearch} />
-      <FlatList
+      <SearchBar value={search} onChangeText={setSearch} handleClear={handleClear}/>
+      {filtered.length === 0 ? (
+        <EmptyState />
+      ) : (
+        <FlatList
         data={filtered}
         keyExtractor={(item) => item.id}
         contentContainerStyle={styles.listContainer}
@@ -38,16 +48,18 @@ export default function CurrencyList() {
                 {item.name.charAt(0).toUpperCase()}
               </Text>
             </View>
-
             <View style={styles.nameContainer}>
               <Text style={styles.nameText}>{item.name}</Text>
             </View>
 
             <Text style={styles.symbolText}>{item.symbol}</Text>
+            <Icon name="chevron-forward" size={20} color="#999" />
 
           </TouchableOpacity>
         )}
       />
+      )}
+
     </View>
   );
 }
